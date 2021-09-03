6f3d09ef95409bb6b5eff442390bc45c8f511b04
JortzFromSchool <johnnydortiz@gmail.com>
2021-09-03 16:16:25 -0400

    two_sum complete

--- two_sum.rb
+++ two_sum.rb
@@ -16,13 +16,43 @@ def okay_two_sum?(arr, target_sum)
     sorted.each_with_index do |ele1, idx1|
         return false if ele1 > target_sum
 
-        return true if sorted.bsearch { |x| x == target_sum - ele1 && arr.index(x) != idx1 }
+        result = bsearch(sorted, target_sum - ele1)
+        return true unless result == idx1 || result == nil 
     end
     false
+end
+
+def bsearch(arr, target)
+    return nil if arr.length == 0
+    middle = arr.length/2
+    return middle if arr[middle] == target
+    bsearch(arr.take(middle), target) if target < arr[middle]
+    if target > arr[middle]
+        result = bsearch(arr.drop(middle+1), target)
+        return result.nil? ? nil : middle + 1 + result
+    end
+    nil
+end
+
+
+#O(n)
+def good_two_sum?(arr, target_sum)
+    hash = Hash.new(0)
+    arr.each do |ele|
+        hash[ele] += 1
+    end
+
+    hash.each_key do |key|
+        hash[key] -= 1
+        return true if hash.has_key?(target_sum - key) && hash[target_sum - key] > 0
+    end
+    return false
 end
 
 arr = [0, 1, 5, 7]
 # p two_sum?(arr, 6) # => should be true
 # p two_sum?(arr, 10) # => should be false
-p okay_two_sum?(arr, 6) # => should be true
-p okay_two_sum?(arr, 10) # => should be false
+# p okay_two_sum?(arr, 6) # => should be true
+# p okay_two_sum?(arr, 10) # => should be false
+p good_two_sum?(arr, 6) # => should be true
+p good_two_sum?(arr, 10) # => should be false
